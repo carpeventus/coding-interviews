7c5c9dc69c6a8f5d51c3290341a647a75545ac49
sunhaiyu <haiyu19931121@163.com>
2018-08-24 23:47:29 +0800

    更正了错误代码

--- code/offer/src/Chap7/LastSameInBST.java
+++ code/offer/src/Chap7/LastSameInBST.java
@@ -17,9 +17,9 @@ public class LastSameInBST {
         Node cur = root;
         while (cur != null) {
             if (cur.val < a.val && cur.val < b.val) {
-                cur = cur.left;
-            } else if (cur.val > a.val && cur.val > b.val) {
                 cur = cur.right;
+            } else if (cur.val > a.val && cur.val > b.val) {
+                cur = cur.left;
             } else {
                 return cur;
             }
