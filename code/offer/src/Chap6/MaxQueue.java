8ca9cf0db847d02f40ed819ecfe1130dbde5608a
sunhaiyu <haiyu19931121@163.com>
2021-07-02 09:12:48 +0800

    Update MaxQueue.java
    修复MaxQueue错误

--- code/offer/src/Chap6/MaxQueue.java
+++ code/offer/src/Chap6/MaxQueue.java
@@ -1,6 +1,7 @@
 package Chap6;
 
 import java.util.Deque;
+import java.util.Queue;
 import java.util.LinkedList;
 
 /**
@@ -8,49 +9,42 @@ import java.util.LinkedList;
  * 要求入列、出列以及邱最大值的方法时间复杂度都是O(1)
  */
 public class MaxQueue {
-    private Deque<Integer> maxDeque = new LinkedList<>();
-    private Deque<Integer> dataDeque = new LinkedList<>();
 
-    public void offer(int number) {
-        dataDeque.offerLast(number);
-        // 即将要存入的元素比当前队列最大值还大，存入该元素
-        if (maxDeque.isEmpty() || number > maxDeque.peekFirst()) maxDeque.offerFirst(number);
-        // 即将要存入的元素不超过当前队列最大值，再将最大值存入一次
-        else maxDeque.offerFirst(maxDeque.peekFirst());
+    private Queue<Integer> queue;
+    private Deque<Integer> maxQueue;
+
+
+    public MaxQueue() {
+        queue = new LinkedList<>();
+        maxQueue = new LinkedList<>();
     }
 
-    public void poll() {
-        if (dataDeque.isEmpty()) throw new RuntimeException("队列已空");
-        dataDeque.pollFirst();
-        maxDeque.pollFirst();
+    public int max_value() {
+        if (maxQueue.isEmpty()) {
+            return -1;
+        }
+        return maxQueue.peekFirst();
     }
 
-    public int max() {
-        if (maxDeque.isEmpty()) throw new RuntimeException("队列已空");
-        return maxDeque.peekFirst();
-    }
-
-    public static void main(String[] args) {
-        MaxQueue maxQueue = new MaxQueue();
-        maxQueue.offer(2);
-        maxQueue.offer(3);
-        maxQueue.offer(4);
-        maxQueue.offer(2);
-        maxQueue.offer(6);
-        maxQueue.offer(2);
-        maxQueue.offer(5);
-        maxQueue.offer(1);
-
-        System.out.println(maxQueue.max());
-        maxQueue.poll();
-        maxQueue.poll();
-        maxQueue.poll();
-        maxQueue.poll();
-        System.out.println(maxQueue.max());
-        maxQueue.poll();
-        System.out.println(maxQueue.max());
-        maxQueue.poll();
-        System.out.println(maxQueue.max());
+    public void push_back(int value) {
+        queue.offer(value);
+        while (!maxQueue.isEmpty() && value > maxQueue.peekLast()) {
+            maxQueue.pollLast();
+        }
+        maxQueue.offerLast(value);
 
     }
+
+    public int pop_front() {
+        if (queue.isEmpty()) {
+            return -1;
+        }
+        int tmp =  queue.poll();
+        if (tmp == maxQueue.peekFirst()) {
+            maxQueue.pollFirst();
+        }
+        return tmp;
+    }
+
 }
+
